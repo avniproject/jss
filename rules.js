03375d3af0b22b753b5ef48ed4623fda2c8d800a
Arjun <arjunk@thoughtworks.com>
2018-08-24 14:50:09 +0530

    Filtering grampanchayat based on village selected

--- rules.js
+++ rules.js
@@ -101,11 +101,86 @@ class GMCancelVisitScheduleJSS {
     }
 }
 
+const VILLAGE_GRAMPANCHAYAT_MAPPING = new Map([["Payari", "Tarang"],
+    ["Tarang", "Tarang"],
+    ["Katurdona", "Khamroud"],
+    ["Khalebhanvar", "Khamroud"],
+    ["Khamroud Village", "Khamroud"],
+    ["Tankitola", "Khamroud"],
+    ["Chatua", "Khamroud"],
+    ["Piparha", "Khamroud"],
+    ["Tarera", "Ahirgawan"],
+    ["Sital pani", "Ahirgawan"],
+    ["Sarai", "Sarai"],
+    ["Mithu mahua", "Mithu mahua"],
+    ["Tithi", "Mithu mahua"],
+    ["Jaithari", "Mithu mahua"],
+    ["Dona", "Mithu mahua"],
+    ["Daldali", "Ahirgawan"],
+    ["Khumni", "Khumni"],
+    ["BulhuPani", "Khumni"],
+    ["Dadra Silvari", "Kodar"],
+    ["Neeche Silvari", "Kodar"],
+    ["Kodar Village", "Kodar"],
+    ["Sarfa", "Sarfa"],
+    ["Khirni", "Sarfa"],
+    ["Ahirgawan", "Ahirgawan"],
+    ["Badi Tumi", "Badi Tumi"],
+    ["Thunguni", "Duradhar"],
+    ["Duradhar", "Duradhar"],
+    ["Kharsol", "Kharsol"],
+    ["Chirpani", "Badi Tumi"],
+    ["Dabhaniya", "Badi Tumi"],
+    ["Majhauli", "Kharsol"],
+    ["Kui", "Padmania"],
+    ["Padmania Village", "Padmania"],
+    ["Gijri", "Gijri"],
+    ["Amoda", "Gijri"],
+    ["Batiya", "Duradhar"],
+    ["Bijora", "Gijri"],
+    ["Khosurgod", "Sarai"],
+    ["Chottibairagi", "Alwar"],
+    ["Badibairagi", "Alwar"],
+    ["Alwar Village", "Alwar"],
+    ["Adwar", "Bijapuri 2"],
+    ["Lamsarai", "Lamsarai"],
+    ["Ghata", "Alwar"],
+    ["Hathbandha", "Bijapuri 2"],
+    ["Jairhi", "Bijapuri 2"],
+    ["Karoundi", "Lamsarai"],
+    ["temp1", "Ataria"],
+    ["temp2", "Bijouri"],
+    ["temp3", "Chandania"],
+    ["temp4", "Chiliamar"],
+    ["temp5", "Dhanpuri"],
+    ["temp6", "Dudhmania"],
+    ["temp7", "Gonda"],
+    ["temp8", "Karpa"],
+    ["temp9", "Ledra"],
+    ["temp10", "Nagula"],
+    ["temp11", "Nogawan"],
+    ["temp12", "Padri"],
+    ["temp13", "Padri"],
+    ["temp14", "Ufrikala"]
+
+]);
 @RegistrationViewFilter("c2e89483-4bdd-4d39-adf3-88a69579d07d", "JSS Registration View Filter", 1.0, {})
 class RegistrationHandlerJSS {
     static exec(individual, formElementGroup) {
         return FormElementsStatusHelper
             .getFormElementsStatuses(new RegistrationHandlerJSS(), individual, formElementGroup);
+    }
+
+    gramPanchayat(individual, formElement) {
+        const statusBuilder = this._getStatusBuilder(individual, formElement);
+        var villageGrampanchayatMappingClone = new Map(VILLAGE_GRAMPANCHAYAT_MAPPING);
+        var notToRemove = villageGrampanchayatMappingClone.get(individual.lowestAddressLevel.name);
+        villageGrampanchayatMappingClone.delete(individual.lowestAddressLevel.name);
+        var oldflatten = _.flatten([...villageGrampanchayatMappingClone.values()]).filter((p) => !_.isEmpty(p));
+
+        const flatten = oldflatten.filter((val) => val !== notToRemove);
+        statusBuilder.skipAnswers.apply(statusBuilder, flatten);
+        return statusBuilder.build();
     }
 
     otherGramPanchayatPleaseSpecify(individual, formElement) {

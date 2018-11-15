759bae7b3045713ab172bd99fca7be40b70d47bf
Sidtharthan A N <sidthunagarajan@gmail.com>
2018-11-15 13:23:12 +0530

    Update Makefile for release 2.6. Use a generified version.
    Though jss is not deployed on 2.6, this upgrade is needed to deploy-dev to test a visit schedule bug locally

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const {postAllRules} = require("rules-config/infra");
 const _ = require('lodash');
 const serverurl = _.isEmpty(process.argv[2]) ? 'http://localhost:8021' : process.argv[2];
 const token = _.isEmpty(process.argv[3]) ? '' : process.argv[3];
-postAllRules("JSS", "./rules.js", serverurl, token);
+const orgAdmin = _.isEmpty(process.argv[4]) ? '' : process.argv[4];
+postAllRules(orgAdmin, "./rules.js", serverurl, token);

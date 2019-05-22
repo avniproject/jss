538ce2f0760c7e810d5d1d2f4018dab61ab64013
sidtharthan <sidthunagarajan@gmail.com>
2019-05-22 12:46:26 +0530

    1418 | Make sample catchments refer to sample locations instead of locations from openchs as they are removed
    Also make use of openchs sdk

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+const rulesConfigInfra = require('rules-config/infra');
+const IDI = require('openchs-idi');
+const secrets = require('../secrets.json');
+
+module.exports = IDI.configure({
+    "name": "jss",
+    "chs-admin": "admin",
+    "org-name": "JSS",
+    "org-admin": "adminjss",
+    "secrets": secrets,
+    "files": {
+        "adminUsers": {
+            // "prod": [],
+            "dev": ["./users/admin-user.json"],
+        },
+        "forms": [
+            "./child/albendazoleTrackingForm.json",
+            "./registrationForm.json",
+        ],
+        "formMappings": ["./formMappings.json"],
+        "formDeletions": [
+            "./child/enrolmentDeletions.json",
+        ],
+        "formAdditions": [
+            "./child/enrolmentAdditions.json",
+        ],
+        "catchments": ["./catchments/sample.json"],
+        "checklistDetails": [],
+        "concepts": [
+            "./concepts.json",
+            "./registrationConcepts.json",
+            "./child/enrolmentConcepts.json",
+        ],
+        "locations": [
+            "./locations/sample.json",
+        ],
+        "programs": [],
+        "encounterTypes": ["./encounterTypes.json"],
+        "operationalEncounterTypes": ["./operationalModules/operationalEncounterTypes.json"],
+        "operationalPrograms": ["./operationalModules/operationalPrograms.json"],
+        "operationalSubjectTypes": ["./operationalModules/operationalSubjectTypes.json"],
+        "users": {
+            "dev": ["./users/test-users.json"]
+        },
+        "rules": [
+            "./rules.js",
+        ],
+        "organisationSql": [
+            /* "create_organisation.sql"*/
+        ]
+    }
+}, rulesConfigInfra);

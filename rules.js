c2462865c79739cc51365ae12c11aa731adf9426
sachin kadam <sachinkadamsk01@gmail.com>
2021-06-07 12:41:29 +0530

    #41 | phulwari changes

--- rules.js
+++ rules.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 
 module.exports = _.merge({},
-    require('./registration/rules/registrationHandler'),
-    require('./shared/rules/cancelVisitsHandler'),
+    // require('./registration/rules/registrationHandler'),
+    // require('./shared/rules/cancelVisitsHandler'),
     require('./child/rules/enrolmentHandler'),
-    require('./child/rules/gmpHandler'),
-    require('./child/rules/albendazoleTrackingHandler'),
-    require('./child/rules/showHeightHandler'),
+    // require('./child/rules/gmpHandler'),
+    // require('./child/rules/albendazoleTrackingHandler'),
+    // require('./child/rules/showHeightHandler'),
 );

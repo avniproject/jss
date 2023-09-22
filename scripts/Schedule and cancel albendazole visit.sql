bda50039161b382bb8eb8a9f8abf21ca95910be6
Md-Taqi <94247708+Gojo-Taqi@users.noreply.github.com>
2023-09-22 19:21:01 +0530

    Update Schedule and cancel albendazole visit.sql

--- scripts/Schedule and cancel albendazole visit.sql
+++ scripts/Schedule and cancel albendazole visit.sql
@@ -22,21 +22,19 @@ with to_be_scheduled as (
 	select 
 	pe.address_id 		add_id,
 	pe.individual_id 	ind_id ,
-	pe.created_by_id 	cre_id
+	pe.created_by_id 	cre_id,
+	pe.program_enrolment_id 	program_enrolment_id 
 	from program_encounter pe 
 	where organisation_id = 11
 	and encounter_type_id = 42
-	and cancel_date_time is null
-	and encounter_date_time is null
-	and earliest_visit_date_time::timestamp <= '2023-03-01 00:00:00.000 +0530'::timestamp
 )
 insert into program_encounter (
 observations, encounter_date_time, encounter_type_id, individual_id, uuid,
 "version", organisation_id, is_voided, audit_id, earliest_visit_date_time,
 max_visit_date_time, cancel_date_time, cancel_observations, "name", created_by_id,
-last_modified_by_id, created_date_time, last_modified_date_time, address_id)
+last_modified_by_id, created_date_time, last_modified_date_time, address_id, program_enrolment_id)
 select '{}'::jsonb, null, 42, ind_id, uuid_generate_v4(),
 0, 11, false, create_audit(), '2023-08-01 00:00:00.000 +0530'::timestamp,
 '2023-08-30 00:00:00.000 +0530'::timestamp, null, '{}'::jsonb, 'Albendazole AUG', cre_id,
-cre_id, now(), now(), add_id
+cre_id, now(), now(), add_id, program_enrolment_id
 from to_be_scheduled;
